b5cb0c3099570abb77c27bb201d5b805a1714b0b
Justin Maldonado <jemaldonado@cs.stonybrook.edu>
2017-05-30 20:09:11 -0400

    Wrote task to read from cache (untested)

--- src/cache.sv
+++ src/cache.sv
@@ -6,33 +6,93 @@
 
 module cache
 (
+  input clk,
+  input reset, 
+
   // Memoory Read Request Interface
   input bus_respcyc,
-  input bus_reqack,
+  output bus_respack,
   input [BUS_DATA_WIDTH - 1 : 0] bus_resp,
   input [BUS_TAG_WIDTH - 1 : 0] bus_resptag,
 
   // Memory Write Request interface
   output bus_reqcyc,
-  output bus_respack,
+  input bus_reqack,
   output [BUS_DATA_WIDTH - 1 : 0] bus_req,
   output [BUS_TAG_WIDTH - 1 : 0] bus_reqtag,
 
   // Instruction Read Request
+  input instr_read,
   input [`ADDRSIZE - 1 : 0] instruction_address,
   output [`INSTRSIZE - 1 : 0] instruction_repsonse,
   output instruction_busy,
 
   // Data Request
-  input [`ADDRSIZE - 1 : 0] data_address,
   input mem_read,
   input mem_write,
-  output [`INSTRSIZE - 1 : 0] data_repsonse,
+  input [`ADDRSIZE - 1 : 0] data_address,
+  output [`DATASIZE - 1 : 0] data_repsonse,
   output data_busy
 );
 
+  data_cache_block [`WAYS - 1 : 0] data_way;
+  instr_cache_block [`WAYS - 1 : 0] instr_way;
+  
+  task reset_signals;
+    begin
+      data_busy = 0;
+      data_response = 0;
+
+      instruction_busy = 0;
+      instruction_response = 0;
+    end
+  endtask
+  
+  task read;
+    input [`ADDRSIZE - 1 : 0] address;
+    output success;
+    output [`DATASIZE - 1 : 0] val;
+    begin
+      cache_instruction addr = address;
+      logic success = 0;
+
+      for (int i = 0; i < `INDEXSIZE; i++) begin
+        if (addr.tag == data_way[i].tag) begin
+          success = 1;
+          value = data_way[i].data_cells[addr.tag];
+        end 
+        if (addr.tag == instr_way[i].tag) begin
+          success = 1;
+          value = instr_way[i].data_cells[addr.tag];
+        end 
+      end
+    end
+  endtask
 
 
+  // Make request to Memory
+  always_comb begin
+    if (!reset) begin
+      if (mem_read ^ mem_write) begin
+        // Send a data read request
+      end else if (instr_read) begin
+        // Check cache
+
+
+        // Send an instruction read request
+        bus_req = instruction_address;
+        bus_reqtag = `MEMR;
+        bus_reqcyc = 1;
+      end
+    end
+  end
+
+  // Make Reset Signals when request returns
+  always_ff @(posedge clk) begin
+    bus_respack <= 0;
+  end
+
+  // Insert into cache and return to processor
   always_comb begin
   end
 endmodule
--- src/consts.sv
+++ src/consts.sv
@@ -4,17 +4,41 @@
 `define DATASIZE      64
 `define DIRTY          1
 `define INSTRSIZE     32
+`define MEMR     0'h1100
 `define OFFSETSIZE     6
+`define SETSIZE        4
 `define TAGSIZE       56
 `define VALID          1
+`define WAYS           4
+
 
 // CACHE CONSTANTS
-typedef logic [`DATASIZE - 1 : 0] data;
+typedef struct packed {
+   logic [`TAGSIZE - 1 : 0] tag;
+   logic [`SETSIZE - 1 : 0] set;
+   logic [`OFFSETSIZE - 1 : 0] offset;
+} cache_instruction;
+
 typedef struct packed {
    logic valid;
    logic dirty;
    logic [`TAGSIZE - 1 : 0] tag;
-   data [`OFFSETSIZE - 1 : 0] data_cells;
 } cache_line;
 
-typedef cache_line[`BLOCKSPERWAY - 1 : 0] cache_block;
+// Data Cache Block
+typedef logic [`DATASIZE - 1 : 0] data;
+typedef struct packed {
+  cache_line cache;
+  data [`OFFSETSIZE - 1 : 0] data_cells;
+} data_cache_line;
+typedef data_cache_line[`BLOCKSPERWAY - 1 : 0] data_cache_block;
+
+// Instruction cache block
+typedef logic [`INSTRSIZE - 1 : 0] instr;
+typedef struct packed {
+  cache_line cache;
+  instr [`OFFSETSIZE - 1 : 0] instr_cells;
+} instr_cache_line;
+typedef instr_cache_line[`BLOCKSPERWAY - 1 : 0] instr_cache_block;
+
+
